69cbc7893840c1cf76c2eec24c6357b7e8ebfcc2
RomanRumania <153564445+Holifkly@users.noreply.github.com>
2024-02-12 16:48:28 -0600

    Update script.js

--- 2.0/script.js
+++ 2.0/script.js
@@ -11,14 +11,12 @@ modeToggle.addEventListener('click', function() {
   modeToggle.textContent = currentMode;
 });
 
-// Check local storage for mode preference
 const savedMode = localStorage.getItem('mode');
 if (savedMode === 'dark') {
   body.classList.add('dark-mode');
   modeToggle.textContent = 'Change it to Light Mode';
 }
 
-// Save mode preference to local storage
 modeToggle.addEventListener('click', function() {
   if (body.classList.contains('dark-mode')) {
     localStorage.setItem('mode', 'dark');
@@ -27,25 +25,21 @@ modeToggle.addEventListener('click', function() {
   }
 });
 
-// Open modal with terms of service
 window.onload = function() {
   modal.style.display = 'block';
   termsFrame.src = 'https://raw.githubusercontent.com/Holifkly/Holifkly/main/terms.txt';
 };
 
-// Close the modal when user clicks on close button
 closeBtn.onclick = function() {
   modal.style.display = 'none';
 };
 
-// Close the modal when user clicks outside of it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = 'none';
   }
 };
 
-// Agree to terms and hide the modal
 agreeButton.onclick = function() {
   modal.style.display = 'none';
 };
